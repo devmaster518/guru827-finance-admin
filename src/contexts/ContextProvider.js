8481fcdcc808629d410ddab8b722cf33758a25e5
devmaster518 <christendennis779@gmail.com>
2024-07-02 22:08:06 +0200

    revert default theme mode to Dark

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
@@ -11,25 +11,26 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [screenSize, setScreenSize] = useState(undefined);
-  const [currentColor, setCurrentColor] = useState('#03C9D7');
-  const [currentMode, setCurrentMode] = useState('Light');
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Dark");
   const [themeSettings, setThemeSettings] = useState(false);
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
 
   const setMode = (e) => {
     setCurrentMode(e.target.value);
-    setThemeSettings(prev => !prev)
-    localStorage.setItem('themeMode', e.target.value);
+    setThemeSettings((prev) => !prev);
+    localStorage.setItem("themeMode", e.target.value);
   };
 
   const setColor = (color) => {
     setCurrentColor(color);
-    setThemeSettings(prev => !prev)
-    localStorage.setItem('colorMode', color);
+    setThemeSettings((prev) => !prev);
+    localStorage.setItem("colorMode", color);
   };
 
-  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+  const handleClick = (clicked) =>
+    setIsClicked({ ...initialState, [clicked]: true });
 
   return (
     <StateContext.Provider
@@ -49,7 +50,7 @@ export const ContextProvider = ({ children }) => {
         setMode,
         setColor,
         themeSettings,
-        setThemeSettings
+        setThemeSettings,
       }}
     >
       {children}
